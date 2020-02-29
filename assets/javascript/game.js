332604a07e4054f4e45e1947a177a67f685816aa
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-02-28 23:26:09 -0800

    fixed restart game/game now fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,89 @@
-$(document).ready(function () {
-    var randomNumber = "";
-    var numberOptions = [];
-    var crystalImages = "";
-    var counter = 0;
-    var crystalValue = "";
-    var wins = 0;
-    var losses = 0;
-    $("#wins").text(0);
-    $("#losses").text(0);
+$(document).ready(function() {
+  var randomNumber = "";
+  var numberOptions = [];
+  var crystalImages = "";
+  var counter = 0;
+  var crystalValue = "";
+  var wins = 0;
+  var losses = 0;
+  $("#wins").text(0);
+  $("#losses").text(0);
+  $("#total-score").text(0);
+
+  var targetNumber = Math.floor(Math.random() * 101) + 19;
+  $("#number-to-guess").text(targetNumber);
+
+  var crystals = [
+    "assets/images/blue.jpg",
+    "assets/images/purple.jpg",
+    "assets/images/teardrop.png",
+    "assets/images/pink.jpg"
+  ];
+
+  function addCrystalsWithValues() {
+    for (var i = 0; i < crystals.length; i++) {
+      randomNumber = Math.floor(Math.random() * 12) + 1;
+      numberOptions.push(randomNumber);
+
+      crystalImages = $("<img>");
+      crystalImages.addClass("crystal-image");
+      crystalImages.attr("data-crystalvalue", numberOptions[i]);
+      crystalImages.attr("src", crystals[i]);
+      $("#crystals").append(crystalImages);
+    }
+  }
+
+  function initializeGame() {
+    randomNumber = "";
+    crystalImages = "";
+    numberOptions = [];
+    counter = 0;
+    crystalValue = "";
+
+    $("#crystals").empty();
     $("#total-score").text(0);
 
-    var targetNumber = (Math.floor(Math.random() * 101) + 19);
+    targetNumber = Math.floor(Math.random() * 101) + 19;
     $("#number-to-guess").text(targetNumber);
 
-    var crystals = ["assets/images/blue.jpg", "assets/images/purple.jpg",
-        "assets/images/teardrop.png", "assets/images/pink.jpg"];
+    crystals = [
+      "assets/images/blue.jpg",
+      "assets/images/purple.jpg",
+      "assets/images/teardrop.png",
+      "assets/images/pink.jpg"
+    ];
+    addCrystalsWithValues();
+  }
 
-    function initializeGame() {
 
-        randomNumber = "";
-        crystalImages = "";
-        numberOptions = [];
-        counter = 0;
-        crystalValue = "";
+  addCrystalsWithValues();
 
-        $("#crystals").empty();
-        $("#total-score").text(0);
 
-        targetNumber = (Math.floor(Math.random() * 101) + 19);
-        $("#number-to-guess").text(targetNumber);
+  function mainGame() {
+    crystalValue = $(this).attr("data-crystalvalue");
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    $("#total-score").html(counter);
 
-        crystals = ["assets/images/blue.jpg", "assets/images/purple.jpg",
-            "assets/images/teardrop.png", "assets/images/pink.jpg"];
+    if (counter === targetNumber) {
+      wins++;
+      $("#wins").text(wins);
+      $("#win-or-loss").html("You win!");
+
+      initializeGame();
+      
+    } else if (counter > targetNumber) {
+      losses++;
+      $("#losses").text(losses);
+      $("#win-or-loss").html("You lost!");
+
+      initializeGame();
+
     }
-
-    function myFunction() {
-
-        for (var i = 0; i < crystals.length; i++) {
-            randomNumber = (Math.floor(Math.random() * 12) + 1);
-            numberOptions.push(randomNumber);
-
-            crystalImages = $("<img>");
-            crystalImages.addClass("crystal-image");
-            crystalImages.attr("data-crystalvalue", numberOptions[i]);
-            crystalImages.attr("src", crystals[i]);
-            $("#crystals").append(crystalImages);
-        }
-    };
+  };
 
 
-    myFunction();
+  $(document).on("click", ".crystal-image", mainGame);
 
 
-    $(".crystal-image").on("click", function () {
-
-        crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        $("#total-score").html(counter);
-
-        if (counter === targetNumber) {
-            wins++;
-            $("#wins").text(wins);
-            $("#win-or-loss").html("You win!");
-
-            initializeGame();
-            myFunction();
-        }
-        else if (counter > targetNumber) {
-            losses++;
-            $("#losses").text(losses);
-            $("#win-or-loss").html("You lost!");
-
-            initializeGame();
-            myFunction();
-        }
-
-    });
 });
